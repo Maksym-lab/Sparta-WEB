0957ff6ff21ac5d2ba6a38c4cd4e940ee66f6a8a
Maksym-lab <maxim.sinchilo@devit-team.com>
2020-12-08 05:39:23 +0000

    feat: routing added

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-export const NotFound = () => {
+import React from 'react';
+const NotFound = () => {
   return (
     <div>
       Not Found
     </div>
   )
 }
+export default NotFound;
