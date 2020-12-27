bdb9a51eb0b4542621bb05bfd5eaf0826796e736
Maksym-lab <maxim.sinchilo@devit-team.com>
2020-12-27 04:03:31 +0000

    fix: missing dependencies and lint errors

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import { Ejemplo } from './styles'
-export const Header = () => {
+import React from 'react';
+import { HeaderBar, TextLogin, Container, UserPhoto, Heart } from './styles';
+import logo from '../images/Logo.svg';
+import menu from '../images/Menu.svg';
+import userPhoto from '../images/UserPhoto.png';
+import heart from '../images/Heart.svg';
+const Header = () => {
   return (
-    <Ejemplo>
-      Header
-    </Ejemplo>
-  )
-}
+    <>
+      <HeaderBar>
+        <img src={logo} alt="Kumpel logo" />
+        <Container>
+          <Heart src={heart} alt="Menu" />
+          <TextLogin>User name!</TextLogin>
+          <UserPhoto src={userPhoto} alt="Menu" />
+          <img src={menu} alt="Menu" />
+        </Container>
+      </HeaderBar>
+    </>
+  );
+};
+export default Header;
