1f9aa94b373c4fb1d468d6d9044db971144fb29f
Maksym-lab <maxim.sinchilo@devit-team.com>
2021-03-14 00:22:25 +0000

    Merge branch '3-create-header-footer-unit-test' into 'staging'

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Header from '../../components/Header';
+describe('<Header />', () => {
+  const header = mount(<Header />);
+  test('Render of Header component', () => {
+    expect(header.length).toEqual(1);
+  });
+});
