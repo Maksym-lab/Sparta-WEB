a9fabd421622a0db80a3aaca50d3c0d50e24eb76
Maksym-lab <maxim.sinchilo@devit-team.com>
2021-03-17 23:48:22 +0000

    feat: Layout created with test

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Router } from '@reach/router';
+import Layout from '../components/Layout';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 const App = () => {
   return (
-    <Router>
-      <NotFound default />
-      <Home path="/" />
-    </Router>
+    <Layout>
+      <Router>
+        <NotFound default />
+        <Home path="/" />
+      </Router>
+    </Layout>
   );
 };
 export default App;
