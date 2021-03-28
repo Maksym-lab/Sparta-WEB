d8920e6da303bcbbd050381ec62098613df89b5f
Maksym-lab <maxim.sinchilo@devit-team.com>
2021-03-28 21:49:31 +0000

    fix: import error fixed

--- src/__test__/components/Layout.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import Layout from '../../components/Layout';
-describe('<Layout />', () => {
-  const layout = mount(<Layout />);
-  test('Render of Layout component', () => {
-    expect(layout.length).toEqual(1);
-  });
-});
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Router } from '@reach/router';
-import Layout from '../components/Layout';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 const App = () => {
   return (
-    <Layout>
-      <Router>
-        <NotFound default />
-        <Home path="/" />
-      </Router>
-    </Layout>
+    <Router>
+      <NotFound default />
+      <Home path="/" />
+    </Router>
   );
 };
 export default App;
