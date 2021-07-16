46c2281ce2fe93c67aa76887bd37985c7572e30e
Maksym-lab <maxim.sinchilo@devit-team.com>
2021-07-16 14:09:23 +0000

    Merge branch '24-create-modal-component' into 'staging'

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { MdPanoramaFishEye } from 'react-icons/md';
+import { Hero, Illustration, Page, Description, Button } from './styles';
+import womanIllustration from '../../assets/images/notFoundIllustration.svg';
 const NotFound = () => {
   return (
-    <div>
-      Not Found
-    </div>
+    <Hero>
+      <Illustration>
+        <Page>
+          <div>
+            <MdPanoramaFishEye />
+            <MdPanoramaFishEye />
+            <MdPanoramaFishEye />
+          </div>
+          <hr />
+          <h1>404</h1>
+        </Page>
+        <img src={womanIllustration} alt="" />
+      </Illustration>
+      <Description>
+        <h3>Ooops!</h3>
+        <h2>Page not Found</h2>
+        <p>Sorry the page you are looking for does not exist</p>
+      </Description>
+      <Button>Return</Button>
+    </Hero>
   );
 };
 export default NotFound;
