f58b265499100ee24c3e79d36f3a38127cecd43b
Maksym-lab <maxim.sinchilo@devit-team.com>
2021-07-31 03:26:11 +0000

    Merge branch '28-add-missing-variables' into 'staging'

--- src/__test__/components/EditProfile.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import EditProfile from '../../pages/EditProfile';
-describe('<EditProfile />', () => {
-  const editProfile = mount(<EditProfile />);
-  test('Render of EditProfile page', () => {
-    expect(editProfile.length).toEqual(1);
-  });
-});
