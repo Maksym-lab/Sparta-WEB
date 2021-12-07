05037231ee648e47ba910cea0cdc1ba0c652df3c
Maksym-lab <maxim.sinchilo@devit-team.com>
2021-12-07 12:22:24 +0000

    Merge branch '53-add-room-to-favorites' into 'staging'

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { MdFavorite, MdClose } from 'react-icons/md';
+import InfiniteScroll from 'react-infinite-scroller';
 import MovingBanner from '../../assets/images/MovingBanner.jpg';
 import Badge from '../../components/Badge';
 import Grid from './styles';
 import PreviewCard from '../../components/PreviewCard';
+import Loading from '../../components/Loading';
+import Error from '../../components/Error';
 import { Main, Hero, Section } from '../../components/RoomForm/styles';
-const Favorites = () => {
+import DataNotFound from '../../components/DataNotFound';
+const Favorites = (props) => {
+  const {
+    data: {
+      loading = false,
+      error = false,
+      roomsFavorites = {},
+      loadMore = {},
+    } = {},
+  } = props;
+  if (loading) return <Loading />;
+  if (error) return <Error />;
+  const {
+    hasNext = false,
+    data = [],
+  } = roomsFavorites || {};
+  if (data.length === 0) return <DataNotFound />;
   return (
     <Main>
       <Hero>
@@ -13,48 +32,20 @@ const Favorites = () => {
         <Badge icon={MdFavorite} message="My favorites" />
       </Hero>
       <Section>
-        <Grid>
-          <li>
-            <MdClose />
-            <PreviewCard />
-          </li>
-          <li>
-            <MdClose />
-            <PreviewCard />
-          </li>
-          <li>
-            <MdClose />
-            <PreviewCard />
-          </li>
-          <li>
-            <MdClose />
-            <PreviewCard />
-          </li>
-          <li>
-            <MdClose />
-            <PreviewCard />
-          </li>
-          <li>
-            <MdClose />
-            <PreviewCard />
-          </li>
-          <li>
-            <MdClose />
-            <PreviewCard />
-          </li>
-          <li>
-            <MdClose />
-            <PreviewCard />
-          </li>
-          <li>
-            <MdClose />
-            <PreviewCard />
-          </li>
-          <li>
-            <MdClose />
-            <PreviewCard />
-          </li>
-        </Grid>
+        <InfiniteScroll
+          loadMore={loadMore}
+          hasMore={hasNext}
+        >
+          <Grid aria-label="list of favorites rooms">
+            {data
+              .map(({ id, room }) => (
+                <li key={id}>
+                  <MdClose />
+                  <PreviewCard {...room} />
+                </li>
+              ))}
+          </Grid>
+        </InfiniteScroll>
       </Section>
     </Main>
   );
